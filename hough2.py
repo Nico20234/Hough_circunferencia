81d68e82444bdc52c32b3036cd0b3db63270ebd9
Nico20234 <132843632+Nico20234@users.noreply.github.com>
2023-06-24 20:30:56 -0300

    Update hough2.py

--- hough2.py
+++ hough2.py
@@ -1,29 +1,50 @@
 import cv2
 import numpy as np
 
-# Cargar la imagen
-image = cv2.imread('aro.jpg', cv2.IMREAD_GRAYSCALE)
+# Cargar la imagen en color
+image = cv2.imread('aro_3.jpg')
 
-# Aplicar desenfoque para reducir el ruido
-blurred = cv2.GaussianBlur(image, (5, 5), 0)
+# Convertir la imagen a escala de grises
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Aplicar la transformada de Hough de circunferencias
-circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 20,
-                           param1=50, param2=30, minRadius=0, maxRadius=0)
+# Aplicar el filtro de Canny para detectar los bordes
+edges = cv2.Canny(gray, 50, 130)
 
-# Verificar si se detectaron círculos
+# Aplicar la transformada de Hough para detectar las rectas
+lines = cv2.HoughLines(edges, 1, np.pi/180, threshold=100)
+
+# Filtrar las rectas detectadas
+filtered_lines = []
+for line in lines:
+    rho, theta = line[0]
+    if abs(theta - np.pi/2) > np.pi/6:  # Filtrar rectas que no sean verticales (±30 grados)
+        filtered_lines.append(line)
+
+# Aplicar la transformada de Hough para circunferencias
+circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, dp=1, minDist=50, param1=100, param2=30, minRadius=20, maxRadius=100)
+
+# Dibujar las rectas detectadas en la imagen original
+for line in filtered_lines:
+    rho, theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+    cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+# Dibujar las circunferencias detectadas en la imagen original
 if circles is not None:
-    # Redondear y convertir las coordenadas a enteros
     circles = np.round(circles[0, :]).astype(int)
-
-    # Dibujar los círculos encontrados
     for (x, y, r) in circles:
         cv2.circle(image, (x, y), r, (0, 255, 0), 2)
-        cv2.circle(image, (x, y), 2, (0, 0, 255), 3)
 
-    # Mostrar la imagen con los círculos detectados
-    cv2.imshow('Detected Circles', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-else:
-    print("No se detectaron círculos en la imagen.")
+# Mostrar la imagen original con las rectas y circunferencias detectadas
+cv2.imshow("Detected Lines and Circles", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
